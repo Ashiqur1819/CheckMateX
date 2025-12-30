8cfd336e8ac253b5e051bbf4be8f2754f4aa7a46
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2025-12-30 12:52:50 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
-const express = require("express")
-const socket = require("socket.io")
-const http = require("http")
-const {Chess} = require("chess.js")
-const path = require("path")
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+const { Chess } = require("chess.js");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-const server = http.createServer(app)
-const io = socket(server)
+const server = http.createServer(app);
+const io = socket(server);
 
-const chess = new Chess()
-const players = {}
-const currentPlayer = "W"
+const chess = new Chess();
+const players = {};
+const currentPlayer = "W";
 
-app.set("view engine", "ejs")
-app.use(express.static(path.join(__dirname, "public")))
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.render("index")
-})
+  res.render("index");
+});
 
-io.on('connection', socket => {
-  console.log("Connected")
+io.on("connection", (uniqueSocket) => {
+  console.log("Connected");
+
+  if (!players.white) {
+    players.white = uniqueSocket.id;
+    uniqueSocket.emmit("playerRole", "w");
+  } else if (!players.black) {
+    players.black = uniqueSocket.id;
+    uniqueSocket.emmit("playerRole", "b");
+  } else {
+    uniqueSocket("spectatorRole");
+  }
 });
 
 server.listen(3000, () => {
-    console.log("Server is running on port: 3000")
-})
+  console.log("Server is running on port: 3000");
+});
